118878da4652a60895662f9d450e5352e4a55013
Ekaterina Zolotaeva <e.zolotaeva@yandex.ru>
2020-06-28 14:35:20 +0300

    start

---
+++ js/common.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// let conf = confirm("Are you here?");
+// console.log(conf); 
+
+// const answer = prompt("Вам есть 18?", "есть");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Ваше имя?', '');
+// answers[1] = prompt('Ваша фамилия?', '');
+// answers[2] = prompt('Ваш возраст?', '');
+
+// document.write(answers);
+
+// const category = "toys";
+// //console.log('https://magazin.com/' + category);
+// console.log(`https://magazin.com/${category}/`);
