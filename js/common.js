d72f1f3ff0e4a150d4a709e6852e109139f0a7ec
Katreen <zolotaevaekaterina@gmail.com>
2020-07-21 11:36:30 +0400

    object

--- js/common.js
+++ js/common.js
@@ -1,22 +1,44 @@
 "use strict";
 
-const num = 50;
-//Тернарный оператор
-(num==50) ? console.log('ok!') : console.log('error');
+// function learnJS(lang, callback){
+//       console.log(`я учу ${lang}`);
+//       callback();
+// }
+// function done(){
+//       console.log('я прошел этот урок');
+// }
 
-const uno = 50;
+// learnJS('JavaScript', done);
 
-switch(uno){ //строгое соответствие
-      case 49:
-            console.log('Неверно');
-            break;
-      case 100:
-            console.log('Неверно');
-            break;
-      case 50:
-            console.log('Верно');
-            break;
-      default:
-            console.log('Не в этот раз');
-            break;
-}
+const options = {
+      name: 'test',
+      width: 1024,
+      height: 1024,
+      colors: {
+            border: 'black',
+            bg: 'red'
+      },
+      makeTest: function(){
+            console.log('Test');
+      }
+};
+options.makeTest();
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options).length);
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+// let counter = 0;
+// for(let key in options){
+//       if(typeof(options[key])==='object'){
+//             for(let i in options[key]){
+//                   console.log(`свойство ${i} имеет значение ${options[key][i]}`);    
+//             }
+//       }else{
+//             console.log(`свойство ${key} имеет значение ${options[key]}`);    
+//       }
+//       counter++;
+// }
+// console.log(counter);
